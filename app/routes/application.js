e1a1fd4df9e0112c69b6d076dfc967534bb2c798
coffee-cup <jakerunzer@gmail.com>
2015-04-07 20:25:35 -0700

    load json file and save into chrome sync storage

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
-var scope = {
-  this: null
-}
+// var scope = {
+//   this: null
+// };
 
 export default Ember.Route.extend({
   // setupController: function(controller, model) {
--- public/background.js
+++ public/background.js
@@ -1,6 +1,44 @@
+var schedule = null;
+
 chrome.app.runtime.onLaunched.addListener(function() {
+  loadWindow();
+  loadSchedule();
+});
+
+function saveSchedule() {
+  var json = JSON.stringify(schedule);
+  console.log('saving ' + json);
+  chrome.storage.sync.set({"schedule_key": schedule}, function() {
+    if (chrome.runtime.lastError) {
+      console.log(chrome.runtime.lastError.message);
+      return;
+    }
+
+    console.log('saved schedule');
+    chrome.runtime.sendMessage({message: 'schedule_change'});
+  });
+}
+
+function loadSchedule() {
+  chrome.storage.sync.get("schedule_key", function(data) {
+    if (data.schedule_key) {
+      console.log("found schedule");
+      schedule = data.schedule_key;
+      console.log(schedule);
+      chrome.runtime.sendMessage({message: "schedule_change"});
+    } else {
+      $.getJSON('assets/data/schedule.json', function(data) {
+        console.log('created new schedule');
+        schedule = data.schedule;
+        saveSchedule();
+      });
+    }
+  })
+}
+
+function loadWindow() {
   // Center window on screen.
-  var width = 1000;
+  var width = 800;
   var height = 600;
   var screenWidth = screen.availWidth;
   var screenHeight = screen.availHeight;
@@ -16,4 +54,4 @@ chrome.app.runtime.onLaunched.addListener(function() {
     id: "materialAppID",
     outerBounds: b
   });
-});
+}
