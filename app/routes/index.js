31a34b6c5c767f2649484d856ff200915aba63e4
coffee-cup <jakerunzer@gmail.com>
2015-05-02 13:36:14 -0700

    remembers last class

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,23 @@
 import Ember from 'ember';
 
+var scope = {
+  this: null
+};
+
 export default Ember.Route.extend({
   beforeModel: function() {
     this.transitionTo('class', 'veai5drqq');
+
+    scope.this = this;
+    getSchedule(function(schedule) {
+    // this.transitionTo('class', 'veai5drqq');
+
+      if (schedule.last_class) {
+        scope.this.transitionTo('class', schedule.last_class);
+      } else {
+        scope.this.transitionTo('new');
+      }
+    });
   }
 });
 
