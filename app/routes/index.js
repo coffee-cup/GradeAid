32df1cb5fb0753db786a7b6963efffe175e9f7fe
coffee-cup <jakerunzer@gmail.com>
2015-05-02 17:13:46 -0700

    bug fix and style changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,13 +6,12 @@ var scope = {
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    this.transitionTo('class', 'veai5drqq');
-
     scope.this = this;
     getSchedule(function(schedule) {
     // this.transitionTo('class', 'veai5drqq');
 
       if (schedule.last_class) {
+        console.log(schedule.last_class);
         scope.this.transitionTo('class', schedule.last_class);
       } else {
         scope.this.transitionTo('new');
