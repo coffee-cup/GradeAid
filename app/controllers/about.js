cefdcb14422a066e3a10a8a05e4feecce85d34d9
coffee-cup <jakerunzer@gmail.com>
2015-05-06 17:14:09 -0700

    about page ui setup

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -7,16 +7,36 @@ var scope = {
 
 export default Ember.Controller.extend({
 
+  schedule_import: null,
+
+  import_error: "",
+
+  schedule_string: "",
+
   init: function() {
     this._super();
     scope.this = this;
     scope.newSchedule = newSchedule
   },
 
+  modelUpdate: function() {
+    var schedule = this.get('model');
+    if (schedule) {
+      this.set('schedule_string', JSON.stringify(schedule));
+    }
+  }.observes('model'),
+
   actions: {
     resetSchedule: function() {
       var newSchedule = scope.newSchedule();
       saveSchedule(newSchedule);
+    },
+
+    importSchedule: function() {
+      var newSchedule = this.get('schedule_import');
+      if (newSchedule) {
+
+      }
     }
   }
 });
