c10e951bb81a83cb2ce6e0fc153dda28af74e443
coffee-cup <jakerunzer@gmail.com>
2015-05-09 16:44:18 -0700

    collapsible bug fix

--- public/assets/collapse-init.js
+++ public/assets/collapse-init.js
@@ -5,26 +5,6 @@
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
 
-
-    var editPanel = $('.scotch-panel').scotchPanel({
-      containerSelector: '.mark-edit-panel', /* Finds the closes parent matching this */
-      direction: 'left',
-      duration: 200,
-      distanceX: '100%',
-      enableEscapeKey: true
-    });
-
-    $('#mark-title').dblclick(function(event) {
-      /* Act on the event */
-      console.log('yoyooy');
-      return false;
-    });
-
-    $('.collapsible-header').dblclick(function(event) {
-      /* Act on the event */
-      return false;
-    });
-
   });
 
 })(jQuery); // end of jQuery name space
