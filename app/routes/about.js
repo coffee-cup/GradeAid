9eac841033eb5c8ceb0cd8a4b6398c77b574ca31
coffee-cup <jakerunzer@gmail.com>
2015-05-09 19:49:31 -0700

    ui updates

--- app/routes/about.js
+++ app/routes/about.js
@@ -7,6 +7,7 @@ var scope = {
 export default Ember.Route.extend({
   model: function() {
     scope.this = this;
+    this.controllerFor('about').set('show_buy_us', false);
     getSchedule(function(schedule) {
       scope.this.controllerFor('about').set('model', schedule);
     });
